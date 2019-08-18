6e483b25ddd76c9f7b216feec92909aaec2fc58d
Ante <tolusic9@gmail.com>
2019-08-18 13:55:14 +0200

    Add cleverbot

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const admin = require('firebase-admin')
 admin.initializeApp()
 
 exports.onUserStatusChanged = require('./triggers/onUserStatusChanged')
-
+exports.onCleverbotMessage = require('./triggers/onCleverbotMessage')
 exports.helloWorld = require('./routes/helloWorld')
+
 
